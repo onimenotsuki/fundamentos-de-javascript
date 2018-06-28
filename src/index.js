693dc1115d5f55711ffdf9abf69809dfa063d605
Edgar Talledos <onimenotsuki@gmail.com>
2018-06-27 19:00:08 -0500

    Añadidos ejemplos de definición de funciones

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-// const car = 'Desde archivo car';
-// const carDos = 'Car 2';
+console.log(suma(11, 12));
 
-import car from './car';
+// Notación de Javascript
+// Funciones
+function suma(a, b) {
+  return a + b;
+}
+
+let sumaDos = function(a, b) {
+  return a + b;
+};
+
+let sumaTres = (a, b) => {
+  return a + b;
+};
+
+let sumaCuatro = (a, b) => a + b;
+
+let sumaCinco = (a, b) => (
+  a + b
+);
+
+console.log(sumaDos(12, 13));
+console.log(sumaTres(12, 13));
+console.log(sumaCuatro(12, 13));
+console.log(sumaCinco(12, 13));
